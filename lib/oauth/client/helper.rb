0769191db2ff79fc2e3ecbf138eef456ebb198d5
Beardo Moore <beardo@adobe.com>
2013-05-21 16:10:00 -0600

    Fix nil handling for oauth parameters, again

--- lib/oauth/client/helper.rb
+++ lib/oauth/client/helper.rb
@@ -31,14 +31,14 @@ module OAuth::Client
         'oauth_body_hash'        => options[:body_hash],
         'oauth_callback'         => options[:oauth_callback],
         'oauth_consumer_key'     => options[:consumer].key,
-        'oauth_token'            => options[:token] ? options[:token].token : '',
+        'oauth_token'            => options[:token].try(:token),
         'oauth_signature_method' => options[:signature_method],
         'oauth_timestamp'        => timestamp,
         'oauth_nonce'            => nonce,
         'oauth_verifier'         => options[:oauth_verifier],
         'oauth_version'          => (options[:oauth_version] || '1.0'),
         'oauth_session_handle'   => options[:oauth_session_handle]
-      }.reject { |k,v| v.to_s == "" }
+      }.reject {|k,v| v.nil? }
     end
 
     def signature(extra_options = {})
